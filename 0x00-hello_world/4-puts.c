3c5696a008a69f2d6a6944d3c48beb00d3e09cf8
vdphan <673@holbertonschool.com>
2019-02-06 23:49:23 +0000

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *use put to print a statement
+ *return: alwayls 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+puts;
+return (0);
+}
