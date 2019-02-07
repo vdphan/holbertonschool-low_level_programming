7a9e03a3b8212ff2bdc3698778afa24e292a22c2
vdphan <673@holbertonschool.com>
2019-02-07 00:18:26 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+include <stdio.h>
+/**
+ *main - entry point
+ *use printf to print a statement
+ *Return: alwayls 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
