81f8a0eb6be8abe8dcb828b8a5cdffd5bb50a9a1
vdphan <673@holbertonschool.com>
2019-02-07 01:04:11 +0000

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *charsize: first variable
+ *integersize: second variable
+ *longint: third variable
+ *longlongint: fourth variable
+ *floatsize: fifth variable
+ *Return: alwayls 0 (success)
+ */
+int main()
+{
+	char charsize;
+	int integersize;
+	long int longint;
+	long long int longlongint;
+	float floatsize;
+	/**
+	 *Sizeof operator is used to evaluate the size of a variable
+	 */
+	printf("Size of char: %d byte\n",sizeof(charsize));
+	printf("Size of int: %d bytes\n",sizeof(integersize));
+	printf("Size of int: %ld bytes\n",sizeof(longint));
+	printf("Size of int: %lld bytes\n",sizeof(longlongint));
+	printf("Size of float: %f bytes\n",sizeof(floatsize));
+	return (0);
+}
