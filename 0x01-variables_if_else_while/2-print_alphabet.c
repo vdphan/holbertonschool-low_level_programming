161fb6e44efc1d85675487cbd6765dc2b7e62d6d
vdphan <673@holbertonschool.com>
2019-02-07 22:23:41 +0000

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *use putchar to print alphabet
+ *Return: alwayls 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
