c5223db7e87e58f44ea1077adfcbe7932414016d
vdphan <673@holbertonschool.com>
2019-02-07 01:31:35 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+gcc 6-size.c -m64 -o size64 2> /tmp/64#include <stdio.h>
 /**
  *main - entry point
  *char: first variable
@@ -14,9 +14,9 @@ int main(void)
 	 *Sizeof operator is used to evaluate the size of a variable
 	 */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
