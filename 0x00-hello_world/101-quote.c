e98881ff479ca82aa81df578c2bac84be671ec4d
vdphan <673@holbertonschool.com>
2019-02-07 03:09:42 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  *print error statement
  *Return: alwayls 0 (success)
  */
-int write(int descriptor, char* str, int length);int write(int descriptor, char* str, int length);
+int write(int descriptor, char* str, int length);
 int main (void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 57);
