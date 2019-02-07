21c5a5b8341b1fb933ec0db4ebaaee70c0330760
vdphan <673@holbertonschool.com>
2019-02-07 23:23:49 +0000

    lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *use putchar and for loop to print alphabet
+ *Return: alwayls 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A' ; i <= 'Z' ; i++)
+        {
+                putchar(i);
+        }
+	putchar('\n');
+	return (0);
+}
