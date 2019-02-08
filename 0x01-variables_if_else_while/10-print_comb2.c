ef78d6ceb0a791c4f93d9d7e78d57de67f363bdf
vdphan <673@holbertonschool.com>
2019-02-08 01:51:43 +0000

    print combination

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *use putchar to print 00 to 99.
+ *Return: alwayls 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 99 ; i++)
+	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		if (i < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
