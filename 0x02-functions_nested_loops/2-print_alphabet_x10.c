cb8d585a7cf6ce64c1c04e4537cd141a988ebf1a
vdphan <673@holbertonschool.com>
2019-02-12 09:03:46 +0000

    print 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 /**
- *print all the alphabet
+ *print_alphabet_x10 - print alphabet 10 times
  *void: a function does not return a value
  */
 void print_alphabet_x10(void)
 {
 	int a, i;
+
 	for (a = 1; a <= 10; a++)
 	{
 		for (i = 'a'; i <= 'z'; i++)
