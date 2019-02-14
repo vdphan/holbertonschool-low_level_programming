b1372e937905b4718e2fb53ac3ac3c08d57e58bb
vdphan <673@holbertonschool.com>
2019-02-14 20:10:16 +0000

    print number

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *print_numbers - print number from 1 - 9.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
