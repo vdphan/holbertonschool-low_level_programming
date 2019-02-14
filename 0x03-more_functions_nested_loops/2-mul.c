48e188855e276e423de81fc6596dcb40ce1c27a2
vdphan <673@holbertonschool.com>
2019-02-14 19:58:12 +0000

    multiply

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _isupper - If c is uppercase, return 1. Otherwise, return 0.
+ *@c : character
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
