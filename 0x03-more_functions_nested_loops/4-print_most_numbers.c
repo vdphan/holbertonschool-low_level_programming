675f8f89fd0fd7dd0dcff7ada50f4d423526569e
vdphan <673@holbertonschool.com>
2019-02-14 20:21:39 +0000

    print most numbers

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *print_most_numbers - print number from 1 - 9 except 2 and 4.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		if ((i != 50) && (i != 52))
+			_putchar(i);
+	}
+	_putchar('\n');
+}
