119feec92208ad1cf2cbb78b52872e0df8144a45
vdphan <673@holbertonschool.com>
2019-02-14 01:29:47 +0000

    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,66 @@
+#include "holberton.h"
+/**
+ *print_three - print three digit
+ *@i: integer value
+ *@j: integer value
+ */
+void print_three(int i, int j)
+{
+	_putchar((j * i) / 100 + '0');
+	_putchar((((j * i) % 100) / 10) + '0');
+	_putchar((((j * i) % 100) % 10) + '0');
+}
+/**
+ *print_two - print two digit
+ *@i: integer value
+ *@j: integer value
+ */
+void print_two(int i, int j)
+{
+	_putchar((j * i) / 10 + '0');
+	_putchar((j * i) % 10 + '0');
+}
+/**
+ *print_times_table - print time table
+ *@n: integer value
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	if ((n < 15) && (n > 0))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				if  (j != 0)
+				{
+					_putchar(',');
+					if ((j * i) >= 100)
+					{
+						_putchar(' ');
+					} else if ((j * i) >= 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					} else if ((j * i) < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					}
+				if ((j * i) >= 100)
+					print_three(i, j);
+				else if (((j * i) >= 10) && ((j * i) < 100))
+					print_two(i, j);
+				else
+					_putchar((j * i) % 10 + '0');
+				if ((((j * i) % n) == 0) && (j == n) && (i < n))
+					_putchar('\n');
+			}
+		}
+		_putchar('\n');
+	}
+}
