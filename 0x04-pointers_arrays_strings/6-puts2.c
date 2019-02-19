4de8e652b1d0fcf778dca41e4b8b12dc76ef76f8
vdphan <673@holbertonschool.com>
2019-02-19 21:07:40 +0000

    puts 2

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -7,9 +7,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (((str[i] % 2) == 0) && (str[i] != 0))
+		if (((str[i] % 2) == 0) && (str[i] != '\0'))
 			_putchar(str[i]);
 	}
 	_putchar('\n');
