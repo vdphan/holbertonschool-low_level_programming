b83eac5c3aeedcb464718d64ebfe7f18ea580245
vdphan <673@holbertonschool.com>
2019-02-19 20:42:08 +0000

    puts 2

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * puts2 - prints one char out of 2 of a string.
+ * @str: character value
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		if ((str[i] % 2) == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
