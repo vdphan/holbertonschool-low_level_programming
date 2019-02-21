3e7effac343b68ff449f727a844f1686bdd0b052
vdphan <673@holbertonschool.com>
2019-02-21 21:07:23 +0000

    strncat

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _strncat - concentrate two string with n byte.
+ *@dest: character value.
+ *@src: character value.
+ *@n: integer value.
+ *
+ * Return: dest value.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+		;
+	b = 0;
+	while (src[b] != '\0' && b < n)
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+	b = 0;
+	while (src[b] != '\0' && b >= n)
+	{
+		dest[a] = src[b + n];
+		b++;
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
