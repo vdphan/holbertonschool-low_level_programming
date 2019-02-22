24c3a4e66c4625640a1ce41d6d0afc2da6bd406c
vdphan <673@holbertonschool.com>
2019-02-22 01:16:05 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strcmp -  a function that compare the string.
+ * @s1 : character value
+ * @s2  : character value
+ *
+ *Return: 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+	int k;
+
+	if (s1[i] == s2[j])
+	{
+		i++;
+		j++;
+	}
+	if ((s1[i] != s2[j]) || (*s1 == *s2))
+		k = s1[i] - s2[j];
+	return (k);
+}
