dfaa8dd6b2a5ced530f6e26274f2c730aa3a8360
vdphan <673@holbertonschool.com>
2019-02-25 23:17:39 +0000

    strspn

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *_strspn -  gets the length of a prefix substring.
+ *@s: string character value.
+ *@accept: character value.
+ *
+ * Return: new string value.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int n = 0, k = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				n++;
+		}
+	}
+	k = i - n;
+	return (k);
+}
