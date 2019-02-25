e23bb2d4aeaca880044d320ea9b45437ea890a1f
vdphan <673@holbertonschool.com>
2019-02-25 21:57:33 +0000

    strchr

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strchr -  locates a character in a string.
+ *@s: string character value.
+ *@c: character value.
+ *
+ * Return: string value.
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	return (s = '\0');
+}
