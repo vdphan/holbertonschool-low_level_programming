54a7b374cc216a697169f0e4b969a393e06de2db
vdphan <673@holbertonschool.com>
2019-02-26 07:58:17 +0000

    set string

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * set_string -   sets the value of a pointer to a char.
+ *@s: character value.
+ *@to: character value.
+*/
+void set_string(char **s, char *to)
+{
+	*P = to;
+}
