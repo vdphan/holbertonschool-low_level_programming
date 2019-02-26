c569617c683711bdb977eb4ca679407a48c43d7d
vdphan <673@holbertonschool.com>
2019-02-26 03:43:11 +0000

    strstr

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -8,10 +8,21 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack != '\0'; haystack++)
+	int i;
+	int  j = 0;
+
+	if (needle[j] == '\0')
+		return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (*haystack == *needle)
-			return (haystack);
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i] == needle[j])
+					return (haystack + i);
+			}
+		}
 	}
-	return (0);
+	return(0);
 }
