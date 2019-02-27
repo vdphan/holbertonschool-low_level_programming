e3bd30cd2c757633dc06b61fe4fb1be5d25ab93d
vdphan <673@holbertonschool.com>
2019-02-27 20:06:22 +0000

    puts

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _puts_recursion -  prints a string, followed by a new line.
+ *@s : character value.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	} else
+		_putchar('\n');
+}
