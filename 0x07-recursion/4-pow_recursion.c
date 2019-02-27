80ddbe51652e12ec52c404f52b647190694d71b0
vdphan <673@holbertonschool.com>
2019-02-27 22:01:36 +0000

    power recursion

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - calculate power of the number.
+ *@x: integer value.
+ *@y: integer value.
+ *
+ *Return: the value of x raised to the power of y..
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
