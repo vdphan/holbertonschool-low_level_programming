ddeaa661b1e1c5f93e8c280c92cf6c5f8c111da9
vdphan <673@holbertonschool.com>
2019-02-28 05:00:31 +0000

    add 1 charcter

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -28,7 +28,7 @@ int _strlen_recursion(char *s)
 int palindromehelp(char *s, int i, int j)
 {
 	j = _strlen_recursion(s) - 1;
-	if ((s[i] != s[j]) || j < 1)
+	if ((s[i] != s[j]) || j < 0)
 		return (0);
 	else if (s[i] == s[j])
 		return (1);
