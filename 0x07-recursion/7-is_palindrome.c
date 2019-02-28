a6caf0d291db2a69771591b14d170cd835533260
vdphan <673@holbertonschool.com>
2019-02-28 04:40:28 +0000

    palindrome

---
+++ 0x07-recursion/7-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - returns the length of a string.
+*@s: character value.
+ *
+* Return: the length i of string.
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s)
+	{
+		n = _strlen_recursion(s + 1) + 1;
+
+	}
+	return (n);
+
+}
+/**
+ * palindromehelp - returns square root.
+ *@j: integer value.
+ *@i: integer value.
+ *@s: character value.
+ *
+ * Return: the length i of string.
+ */
+int palindromehelp(char *s, int i, int j)
+{
+	j = _strlen_recursion(s) - 1;
+	if ((s[i] != s[j]) || j < 1)
+		return (0);
+	else if (s[i] == s[j])
+		return (1);
+	return (palindromehelp(s, i + 1, j - 1));
+}
+/**
+ * is_palindrome - check if string is palindrome.
+ *@s: character value.
+ *
+ * Return: 1(palindrome) or 0(not palindrome).
+ */
+int is_palindrome(char *s)
+{
+	return (palindromehelp(s, 0, _strlen_recursion(s) - 1));
+}
