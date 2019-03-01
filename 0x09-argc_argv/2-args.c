22234bca829bdd8a86bdb8675366449bd958507b
vdphan <673@holbertonschool.com>
2019-03-01 07:59:06 +0000

    print received arguments

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *main - entry point of a program that prints all arguments it receives.
+ *@argc: integer value count of the arguments supplied to the program.
+ *@argv: character value an array of pointers to the strings.
+ *
+ *Return: 0(success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
