713f986722e5d7e24ad14116e832e20898cb23ae
vdphan <673@holbertonschool.com>
2019-03-02 01:49:24 +0000

    add positive

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - entry point of a program that adds positive numbers..
+ *@argc: integer value count of the arguments supplied to the program.
+ *@argv: character value an array of pointers to the strings.
+ *
+ *Return: 0(success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (i != 0)
+		{
+			if (atoi(argv[i]) <= 0)
+			{
+				printf("error\n");
+				return (1);
+			}
+			sum = sum + atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
