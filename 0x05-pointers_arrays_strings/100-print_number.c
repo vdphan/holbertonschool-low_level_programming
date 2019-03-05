047e63cf0e42a191f25514775ddfcf7faeb09cc0
vdphan <673@holbertonschool.com>
2019-03-05 06:47:32 +0000

    print integer

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *print_number - print integer number.
+ *@n: integer value.
+ */
+void print_number(int n)
+{
+	unsigned int i;
+
+	i = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	}
+	if (i / 10 != 0)
+		print_number(i / 10);
+	_putchar(i % 10 + '0');
+}
