abe4adc8725098cdaa354c00a1e25baba2d3dead
vdphan <673@holbertonschool.com>
2019-03-05 06:34:07 +0000

    add int-min

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,12 @@ void print_number(int n)
 {
 	unsigned int i;
 
+	i = n;
 	if (n < 0)
 	{
 		_putchar('-');
 		i = -n;
 	}
-	i = n;
 	if (i / 10 != 0)
 		print_number(i / 10);
 	_putchar(i % 10 + '0');
