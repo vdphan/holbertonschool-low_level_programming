bff86e47042e96dd2a1cb4c1102c95775d00a34d
vdphan <673@holbertonschool.com>
2019-03-06 03:22:34 +0000

    first commit

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_grid - concentrate 2 strings.
+ * @grid: integer value.
+ * @height: integer value.
+ *
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
