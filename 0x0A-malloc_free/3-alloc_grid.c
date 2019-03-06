0401d1962f74a6613d19d7df806eb05d9792ee50
vdphan <673@holbertonschool.com>
2019-03-06 01:58:54 +0000

    2 dimensional array

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * alloc_grid - allocate a pointer to a 2 dimensional array of integers.
+ *@width: character value.
+ *@height: character value.
+ *
+ * Return: dest value.
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **array;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	array = malloc(height * sizeof(int *));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+		if (array[i] == NULL)
+		{
+			while (i >= 0)
+				free(array[i]);
+			free(array);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
+	}
+	return (array);
+}
