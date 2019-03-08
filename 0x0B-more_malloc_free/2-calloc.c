899beb29c2cffe020e8813f0be8d431fa07d1b59
vdphan <673@holbertonschool.com>
2019-03-08 00:09:35 +0000

    add smaller than nmemb

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	s = malloc(nmemb * size);
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		s[i] = 0;
 	return (s);
 }
