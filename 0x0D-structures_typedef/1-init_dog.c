6ffaa09c27de14dfcddd133601424412b3a1f5d5
vdphan <673@holbertonschool.com>
2019-03-11 21:46:08 +0000

    Check NULL condition

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -4,7 +4,7 @@
 
 /**
  * init_dog - a function that initialize a variable of type struct dog.
- *@d:
+ *@d: struct string.
  *@name: character value.
  *@age: float value.
  *@owner: character value.
@@ -13,7 +13,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
