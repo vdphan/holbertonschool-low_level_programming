cb5b73655c0dd42d41aae0f2932394aa16793598
vdphan <673@holbertonschool.com>
2019-03-12 00:49:41 +0000

    NULL condition

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -9,9 +9,10 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d->owner == NULL || d->name == NULL)
-		return;
-	free(d->owner);
-	free(d->name);
-	free(d);
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
 }
