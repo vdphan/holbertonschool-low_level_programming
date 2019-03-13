18cb55a86e4df9e741d1ec6ab289a43e5706f3ec
vdphan <673@holbertonschool.com>
2019-03-13 22:07:17 +0000

    shorter version

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -12,8 +12,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (cmp != NULL && array != NULL || size > 0)
+	if (cmp != NULL && array != NULL)
 	{
+		if (size <= 0)
+			return (-1);
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]))
