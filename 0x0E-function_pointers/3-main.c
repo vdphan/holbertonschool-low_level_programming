2070ab4e2578d3ee4ed3289de80334782d139db0
vdphan <673@holbertonschool.com>
2019-03-14 01:59:36 +0000

    first commit

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+/**
+ *main - entry point of a program that adds positive numbers..
+ *@argc: integer value.
+ *@argv: character value.
+ *
+ *Return: 0(success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && argv[3] == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+}
