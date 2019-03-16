78e2a47f296a7725cfe40568b1b821534c5a6958
vdphan <673@holbertonschool.com>
2019-03-16 20:55:25 +0000

    add struct op

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -5,6 +5,17 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	void (*f)(va_list pr);
+} op_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
