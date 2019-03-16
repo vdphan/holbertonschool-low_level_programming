42dc22ae708de3c67df1ef03529817b4e4a80ec7
vdphan <673@holbertonschool.com>
2019-03-16 20:54:09 +0000

    First commit to print all type

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,79 @@
+#include "variadic_functions.h"
+/**
+ * test_char - print char.
+ * @pr: argument pointer.
+ */
+void test_char(va_list pr)
+{
+	printf("%c", va_arg(pr, int));
+}
+/**
+ * test_integer - print char.
+ * @pr: argument pointer.
+ */
+void test_integer(va_list pr)
+{
+	printf("%i", va_arg(pr, int));
+}
+/**
+ * test_float - print char.
+ * @pr: argument pointer.
+ */
+void test_float(va_list pr)
+{
+	printf("%f", va_arg(pr, double));
+}
+/**
+ * test_string - print char.
+ * @pr: argument pointer.
+ */
+void test_string(va_list pr)
+{
+	char *s = va_arg(pr, char*);
+
+	if (!s)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", s);
+}
+/**
+ * print_all -  prints anything.
+ * @format: format type.
+ *
+ * Return: Nothing.
+ */
+void print_all(const char * const format, ...)
+{
+	op_t ops[] = {
+		{"c", test_char},
+		{"i", test_integer},
+		{"f", test_float},
+		{"s", test_string},
+		{NULL, NULL}
+	};
+	va_list pr;
+	int i = 0;
+	int j;
+	char *sep = "";
+
+	va_start(pr, format);
+	while (format[i] && format)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == *ops[j].op)
+			{
+				printf("%s", sep);
+				ops[j].f(pr);
+				sep = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(pr);
+}
