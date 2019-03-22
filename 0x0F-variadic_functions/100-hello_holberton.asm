753ef340a074f5df5d1acd6a27038244c43f2797
vdphan <673@holbertonschool.com>
2019-03-22 02:37:18 +0000

    reduce in 0x80

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,13 @@
-section     .text
-	global      main	;must be declared for linker (ld)
+section .text
+	global   main	;must be declared for linker (ld)
 
 main:				;tell linker entry point
 
-	    mov     edx,len	;message length
-	    mov     ecx,msg	;message to write
-	    mov     ebx,1	;file descriptor (stdout)
-	    mov     eax,4	;system call number (sys_write)
-	    int     0x80	;call kernel
-
-	    mov     eax,1	;system call number (sys_exit)
-	    int     0x80	;call kernel
+	mov     edx,len	;message length
+	mov     ecx,msg	;message to write
+	mov     ebx,1	;file descriptor (stdout)
+	mov     eax,4	;system call number (sys_write)
+	int     0x80	;call kernel
 
 	section     .data
 
