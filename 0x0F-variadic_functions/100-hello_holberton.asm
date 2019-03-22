0e84a594fcd44283790336833d7b74d19dcf9b31
vdphan <673@holbertonschool.com>
2019-03-22 02:26:54 +0000

    first commit

---
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,16 @@
+global main
+
+	section .text
+
+main:
+	  mov rax, 1        	; write(
+	  mov rdi, 1        	;   STDOUT_FILENO,
+	  mov rsi, msg      	;   "Hello, Holberton\n",
+	  syscall           	; );
+
+	  mov rax, 60       	; exit(
+	  mov rdi, 0        	;   EXIT_SUCCESS
+	  syscall           	; );
+
+	section .rodata
+msg:	 db "Hello, Holberton", 10
