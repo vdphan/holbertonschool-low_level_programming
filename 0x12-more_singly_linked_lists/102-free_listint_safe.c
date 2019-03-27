afbd086021e4fb3bbddaf44fdf9b06632022c62e
vdphan <673@holbertonschool.com>
2019-03-27 01:22:00 +0000

    first try

---
+++ 0x12-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe -  frees a listint_t list.
+ * @h: pointer of the pointer that points to a struct.
+ *
+ * Return: the size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	listint_t *tmp;
+
+	while (*h && *h < (*h)->next)
+	{
+		tmp = (*h)->next;
+		free(*h);
+		*h = tmp;
+		count++;
+	}
+	if (*h)
+	{
+		free(*h);
+		count++;
+	}
+	*h = NULL;
+	return (count);
+}
