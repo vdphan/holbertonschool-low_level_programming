f003bb787d46f211e541a3c1aff831b4de9dc13a
vdphan <673@holbertonschool.com>
2019-04-02 21:28:47 +0000

    create a file

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * create_file - a function that creates a file.
+ * @filename: name of the input file.
+ * @text_content: a NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, i;
+
+	if (!*filename | !*text_content)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	for (i = 0; text_content[i]; i++)
+		;
+	w = write(fd, text_content, i);
+	if (w == -1)
+		return (-1);
+	return (1);
+}
