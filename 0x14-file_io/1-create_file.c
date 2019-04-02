c22e10f1b8ee854d33b66ee2082f467d5b43c60c
vdphan <673@holbertonschool.com>
2019-04-02 21:38:13 +0000

    check if text_content is NULL

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -11,15 +11,19 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, w, i;
 
-	if (!*filename | !*text_content)
+	if (!*filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	for (i = 0; text_content[i]; i++)
-		;
-	w = write(fd, text_content, i);
-	if (w == -1)
-		return (-1);
+	if (text_content)
+	{
+		for (i = 0; text_content[i]; i++)
+			;
+		w = write(fd, text_content, i);
+		if (w == -1)
+			return (-1);
+	}
+	close(fd);
 	return (1);
 }
