538cb5c4b830b0e3dc2ccca0af289146b6e609d4
vdphan <673@holbertonschool.com>
2019-04-03 01:42:54 +0000

    add newline

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -13,19 +13,19 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	a = read(file1, buf, 1024);
@@ -33,14 +33,14 @@ int main(int argc, char **argv)
 	{
 		if (write(file2, buf, a) != a)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 		a = read(file1, buf, 1024);
 	}
 	if (close(file2))
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE, followed by a new line, on the POSIX standard error"), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE, followed by a new line, on the POSIX standard error\n"), exit(100);
 	if (close(file1))
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE, followed by a new line, on the POSIX standard error"), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE, followed by a new line, on the POSIX standard error\n"), exit(100);
 	return (0);
 }
