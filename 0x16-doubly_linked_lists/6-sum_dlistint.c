89f139f55b4e1540c5666f81e4786785ba310605
vdphan <673@holbertonschool.com>
2019-05-03 20:26:38 +0000

    sum all node value

---
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - calculate sum of all data.
+ * @head: pointer to head
+ *
+ * Return: the sum of all the data (n) of a dlistint_t linked list.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
