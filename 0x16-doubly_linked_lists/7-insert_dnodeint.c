d48a67939a953430193c377071052b957b329f27
vdphan <673@holbertonschool.com>
2019-05-05 01:42:30 +0000

    add len

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,23 @@
 #include "lists.h"
+#include "lists.h"
 
+/**
+ * dlistint_len - calculate double linked list len.
+ * @h:  pointer to head.
+ *
+ * Return: returns the number of elements in a linked dlistint_t list.
+ */
+size_t len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  * @h: pointer to the pointer that points to head.
@@ -19,25 +37,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	if (idx == 0 || *h == NULL)
+		return (add_dnodeint(h, 0));
+	if (idx == len(*h))
+		return (add_dnodeint_end(h, n));
+	while (tmp)
 	{
-		new->next = *h;
-		new->prev = NULL;
-		*h = new;
-	}
-	else
-	{
-		while (tmp)
+		if (c == (idx - 1))
 		{
-			if (c == (idx - 1))
-			{
-				new->next = tmp->next;
-				tmp->next->prev = new;
-				tmp->next = new;
-				new->prev = tmp;
-			}
-			c++;
-			tmp = tmp->next;
+			new->next = tmp->next;
+			tmp->next->prev = new;
+			tmp->next = new;
+			new->prev = tmp;
 		}
+		c++;
+		tmp = tmp->next;
 	}
 	if (idx > c)
 		return (NULL);
