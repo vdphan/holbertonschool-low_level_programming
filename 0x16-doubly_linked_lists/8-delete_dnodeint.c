abae12f862b0ab0ef7de806cb628a7e0638a6d73
vdphan <673@holbertonschool.com>
2019-05-05 05:33:24 +0000

    check for last node

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -32,22 +32,26 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *hold = NULL;
 	unsigned int count = 0;
 
-	if (!*head)
+	if (!*head || index >= len(*head))
 		return (-1);
 	if (index == 0)
 	{
 		*head = tmp->next;
 		free(tmp);
-		if (len(*head) > 1)
+		if (len(*head) >= 1)
 			(*head)->prev = NULL;
 		return (1);
 	}
-	if (index >= len(*head))
-		return (-1);
 	while (tmp)
 	{
 		if (count == index - 1)
 		{
+			if (!tmp->next->next)
+			{
+				free(tmp->next);
+				tmp->next = NULL;
+				return (1);
+			}
 			hold = tmp->next->next;
 			free(tmp->next);
 			tmp->next = hold;
