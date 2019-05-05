d1219ce0828699edff9b1b7bac34e295a81ee8c1
vdphan <673@holbertonschool.com>
2019-05-05 00:24:21 +0000

    check NULL

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,6 +13,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *end = *head;
 
 	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	if (!*head)
