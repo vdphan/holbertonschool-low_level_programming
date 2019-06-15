b2c124c13253c856e7247ee5deac14bd8e5691e0
vdphan <45059702+vdphan@users.noreply.github.com>
2019-06-14 23:17:46 -0700

    Update 105-main.c

--- 0x1B-sorting_algorithms/105-main.c
+++ 0x1B-sorting_algorithms/105-main.c
@@ -19,12 +19,3 @@ int main(void)
     print_array(array, n);
     return (0);
 }
-alex@/tmp/sort$ gcc -Wall -Wextra -Werror -pedantic 105-main.c 105-radix_sort.c print_array.c -o radix
-alex@/tmp/sort$ ./radix
-19, 48, 99, 71, 13, 52, 96, 73, 86, 7
-
-71, 52, 13, 73, 96, 86, 7, 48, 19, 99
-7, 13, 19, 48, 52, 71, 73, 86, 96, 99
-
-7, 13, 19, 48, 52, 71, 73, 86, 96, 99
-alex@/tmp/sort$
