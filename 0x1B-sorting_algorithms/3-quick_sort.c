9c205d60b6344b38f84bf108b01a04cacbcd7c1a
vdphan <673@holbertonschool.com>
2019-06-17 21:24:49 +0000

    update quick sort

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -1,11 +1,76 @@
 #include "sort.h"
 
 /**
- * quick_sort -
+ * partition - sorts an array of integers using the Quick sort
+ * @arr: array of integer.
+ * @low: lowest value.
+ * @high: highest value.
+ * @size: size of the array.
  *
- * Return: 
+ * Return: i + 1.
+ */
+int partition(int arr[], int low, int high, size_t size)
+{
+	int pivot = arr[high];
+	int i = (low - 1);
+	int j;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (arr[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&arr[i], &arr[j]);
+				print_array(arr, size);
+			}
+		}
+	}
+	if (i + 1 != high)
+	{
+		swap(&arr[i + 1], &arr[high]);
+		print_array(arr, size);
+	}
+	return (i + 1);
+}
+
+/**
+ *qs - recursive function to sort.
+ *@arr: array of integer
+ *@low: lowest value.
+ *@high: highest value.
+ *@size: size of the array.
+ */
+void qs(int arr[], int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pi = partition(arr, low, high, size);
+
+		qs(arr, low, pi - 1, size);
+		qs(arr, pi + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - sorts an array of integers using the Quick sort
+ * @array: array of integer.
+ * @size: size of the array.
  */
 void quick_sort(int *array, size_t size)
 {
+	qs(array, 0, size - 1, size);
+}
 
+/**
+ *swap - swap value between 2 array index.
+ *@a: array index a.
+ *@b: array index b.
+ */
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
