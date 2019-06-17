4dbcfb63670c687fd057ccf628edf6fcaa08e511
vdphan <673@holbertonschool.com>
2019-06-17 23:47:18 +0000

    add shell-sort

--- 0x1B-sorting_algorithms/100-shell_sort.c
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -1,11 +1,29 @@
 #include "sort.h"
 
 /**
- * shell_sort -
- *
- * Return: 
+ * shell_sort - sorts an array of integers with Shell sort - Knuth Sequence.
+ * @array: array of integer.
+ * @size: size of the array.
  */
 void shell_sort(int *array, size_t size)
 {
+	register unsigned int i, j, n = 1;
+	register int val;
 
+	while (n <= size / 3)
+		n = n * 3 + 1;
+	while (n > 0)
+	{
+		for (i = n; i < size; i++)
+		{
+			val = array[i];
+			for (j = i; j > n - 1  && val <= array[j - n]; j -= n)
+			{
+				array[j] = array[j - n];
+			}
+			array[j] = val;
+		}
+		n = (n - 1) / 3;
+		print_array(array, size);
+	}
 }
