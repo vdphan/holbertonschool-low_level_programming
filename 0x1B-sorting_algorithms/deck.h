024509b53cf6e1feb836f13747c604635e2cd6b9
vdphan <673@holbertonschool.com>
2019-06-20 04:29:14 +0000

    first test

---
+++ 0x1B-sorting_algorithms/deck.h
@@ -0,0 +1,61 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+    DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+void insertion_sort_list(deck_node_t **list);
+void swap_l(deck_node_t *left, deck_node_t *right, deck_node_t **list);
+void sort_deck(deck_node_t **deck);
+
+#endif /* DECK_H */
