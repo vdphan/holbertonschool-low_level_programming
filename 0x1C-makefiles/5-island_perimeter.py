b57ca931e109455a53da671f7eae13fe6cfc95a5
vdphan <673@holbertonschool.com>
2019-07-11 22:51:14 +0000

    add 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module creates a function def island_perimeter(grid):
+that returns the perimeter of the island described in grid.
+"""
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid."""
+    p = 0
+    for row in range(len(grid)):
+        for e in range(len(grid[row])):
+            if grid[row][e] == 1:
+                if e == len(grid[row]) - 1 or grid[row][e + 1] == 0:
+                    p += 1
+                if (e != 0 and grid[row][e - 1] == 0) or e == 0:
+                    p += 1
+                if row == 0 or grid[row - 1][e] == 0:
+                    p += 1
+                if row == len(grid) - 1 or grid[row + 1][e] == 0:
+                    p += 1
+    return p
