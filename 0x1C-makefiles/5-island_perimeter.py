bc4bd4ca84ae5165f45414098341ff864ea8ffd2
vdphan <673@holbertonschool.com>
2019-07-11 22:52:31 +0000

    fix pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,6 +3,8 @@
 This module creates a function def island_perimeter(grid):
 that returns the perimeter of the island described in grid.
 """
+
+
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid."""
     p = 0
