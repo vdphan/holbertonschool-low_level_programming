29205a99d33e5621126230b99c21f73e78356486
vdphan <duyphan0@gmail.com>
2019-08-28 19:33:04 +0000

    create print function

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,26 @@
 #include "search_algos.h"
 /**
+ * printer - Prints an array
+ * @array: Target array
+ * @low: Left index of @array
+ * @high: Right index of @array
+ */
+void printer(int *array, size_t low, size_t high)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = low; i <= high; i++)
+	{
+		printf("%d", array[i]);
+		if (i == high)
+			printf("\n");
+		else
+			printf(", ");
+	}
+}
+
+/**
  * binary_search - a function that searches for a value
  * in a sorted array of integers using the Binary search algorithm
  * @array: pointer to the first element of the array to search in
@@ -9,26 +30,17 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i = 0, high = 0, low = 0, mid = 0;
+	size_t high = 0, low = 0, mid = 0;
 
 	if (!array || size == 0)
 		return (-1);
 	high = size - 1;
-	while (1)
+	while (high >= low)
 	{
-		printf("Searching in array: ");
-		for (i = low; i <= high; i++)
-		{
-			if (i != low)
-				printf(", ");
-			printf("%lu", i);
-		}
-		printf("\n");
+		printer(array, low, high);
 		mid = (high + low) / 2;
 		if (array[mid] == value)
 			return (mid);
-		else if (mid == 0 || mid == size -1)
-			return (-1);
 		else if (array[mid] > value)
 			high = mid - 1;
 		else
